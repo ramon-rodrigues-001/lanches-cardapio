e1805062253ac80aa5e7d620641ba9ed0192aa29
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-07-12 11:34:10 -0300

    Adicionei um link da pagina de login com o cardapio

--- Js/javascript.js
+++ Js/javascript.js
@@ -2,10 +2,14 @@ var butao = document.body.querySelector('button#butao')
 butao.addEventListener('click', clicar)
 
 function clicar() {
+    var index_cardapio = document.body.querySelector("a#index-cardapio-01")
+    index_cardapio.href = 'cardapio-01/pagina-01.html'
+
     var nome = document.body.querySelector('input#input-nome')
     var gmail = document.body.querySelector('input#input-gmail')
 
     if (gmail.value == '' || nome.value == '') {
         alert("Favor preencher todos os requisitos")
+        index_cardapio.href = '#'
     }
 }
